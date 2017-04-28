33e73417ae57c41108b4ce556eeb0ea474db871d
Ana Petrova <anastasia.petrova@gmail.com>
2017-04-28 14:20:54 -0700

    XGBoost, basic features, 0.48 score

--- quora_kaggle.py
+++ quora_kaggle.py
@@ -3,6 +3,8 @@ import string
 from sklearn.svm import LinearSVC
 import csv
 from nltk.corpus import stopwords
+import xgboost as xgb
+
 
 punctuation = set(string.punctuation)
 stopwords = stopwords.words("english")
@@ -26,7 +28,7 @@ def write_file(test_f, write_f, model):
         csvreader.next()
         write.write('test_id,is_duplicate\n')
         for row in csvreader:
-            feature_vector = np.asmatrix(text_feature(row[1], row[2]))
+            feature_vector = xgb.DMatrix(np.asmatrix(text_feature(row[1], row[2])))
             result = model.predict(feature_vector)[0]
             write.write('{},{}\n'.format(row[0], result))
 
@@ -40,6 +42,8 @@ def get_words(sentence):
     except AttributeError: # no sentence
         return []
 
+all_bigrams = {}
+counter = 0
 def text_feature(q1, q2):
     '''
     Extract text features
@@ -78,9 +82,24 @@ X_train, y_train = X[:len(X)/2], y[:len(y)/2]
 X_val, y_val = X[len(X)/2:], y[len(y)/2:]
 
 # Train and score model
+'''
 model = LinearSVC(C=1)
 model.fit(X_train, y_train)
 model.score(X_val, y_val)
 print "Score: {}".format(model.score(X_val, y_val))
+'''
+dtrain = xgb.DMatrix(X_train, label=y_train)
+dtest = xgb.DMatrix(X_val, label=y_val)
 
-write_file('test.csv', 'submission.csv', model)
+param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'binary:logistic' }
+param['nthread'] = 4
+param['eval_metric'] = 'auc'
+plst = param.items()
+
+evallist  = [(dtest,'eval'), (dtrain,'train')]
+
+num_round = 10
+bst = xgb.train(plst, dtrain, num_round, evallist)
+
+
+write_file('test.csv', 'submission.csv', bst)
